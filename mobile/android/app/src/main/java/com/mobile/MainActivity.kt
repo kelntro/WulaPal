1bc1901f8f265e72e8860f9a56f860cb3a3f578a
kelntro <michaelentero123@gmail.com>
2025-02-07 21:43:09 +0800

    michael

--- mobile/android/app/src/main/java/com/mobile/MainActivity.kt
+++ mobile/android/app/src/main/java/com/mobile/MainActivity.kt
@@ -1,26 +1,22 @@
-import com.facebook.react.ReactActivity;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
-import android.os.Bundle;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; // Add this line
+package com.mobile
 
-public class MainActivity extends ReactActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null);
-    }
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-    @Override
-    protected String getMainComponentName() {
-        return "mobile"; // Make sure this matches your app name in app.json
-    }
+class MainActivity : ReactActivity() {
 
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new DefaultReactActivityDelegate(
-            this,
-            getMainComponentName(),
-            DefaultNewArchitectureEntryPoint.getFabricEnabled()
-        );
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "mobile"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
