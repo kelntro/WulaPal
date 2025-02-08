82c9383ad6b62de6a10e5f41c7218e594e3fee2e
kelntro <michaelentero123@gmail.com>
2025-02-08 08:05:22 +0800

    Forced add mobile folder

---
+++ mobile/android/app/build/intermediates/cxx/Debug/3q33136q/logs/armeabi-v7a/build_command_appmodules.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Administrator\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Administrator\\WulaPal\\mobile\\android\\app\\.cxx\\Debug\\3q33136q\\armeabi-v7a" ^
+  appmodules
