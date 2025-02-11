7b9b4d58430a8a48e644aa8e6a349de29dcedf2d
kelntro <michaelentero123@gmail.com>
2025-02-11 18:24:43 +0800

    michael

--- mobile/android/app/.cxx/Debug/3q33136q/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Administrator/.gradle/caches/transforms-3/316248f3cb3056071ea537c033ca5dbb/transformed/jetified-hermes-android-0.73.3-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Administrator/.gradle/caches/transforms-3/316248f3cb3056071ea537c033ca5dbb/transformed/jetified-hermes-android-0.73.3-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- mobile/android/app/.cxx/Debug/3q33136q/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Administrator/.gradle/caches/transforms-3/fac93946b7857a5ee69c9949e092f107/transformed/jetified-fbjni-0.5.1/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Administrator/.gradle/caches/transforms-3/fac93946b7857a5ee69c9949e092f107/transformed/jetified-fbjni-0.5.1/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
